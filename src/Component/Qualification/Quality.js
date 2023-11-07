e659ec4e411766ad4b0398dadbd00a7b1f934019
bignemz <chinaemeemerole@yahoo.com>
2023-11-07 12:34:38 +0100

    update

---
+++ src/Component/Qualification/Quality.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+function Quality () {
+
+
+  return (
+  <div>
+
+    <div>
+    <h2>Education</h2>
+    <h2>Experience</h2>
+    </div>
+    <div className="Quality-education">
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+    
+    </div>
+
+
+
+    <div className="Quality-experience">
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+
+   <div className="Quality-item">
+   <div className="Quality-first"></div>
+   <div  className="Quality-second"></div>
+   <div className="Quality-last">
+   <h3>Html,css</h3>
+   <p>Udemy</p>
+   <p><span><i class="fa-regular fa-calendar-check"></i></span>May 2020</p>
+
+   </div>
+   </div>
+    
+    </div>
+  
+    
+  </div>
+  )
+}
+ 
+export default Quality;
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../Component/Navbar/Navbar";
+import Quality from "../../Component/Qualification/Quality";
 
 function Homepage() {
   return (
     <div>
       <Navbar />
+      <Quality/>
     </div>
   );
 }
